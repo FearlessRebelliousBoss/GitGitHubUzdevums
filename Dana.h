a05a26ba6040aec95fa3d9e5d1fa7d1b156f3448
FearlessRebelliousBoss <simplywonderfuld@gmail.com>
2025-05-10 16:34:14 +0300

    Update Dana.h

--- Dana.h
+++ Dana.h
@@ -1,1 +1,19 @@
+#ifndef DANA_H
+#define DANA_H
 
+#include <iostream>
+#include "Name.h"
+using namespace std;
+
+class Dana: public Name
+{
+public:
+    Dana(string = "DefaultName");
+    virtual void setName(string);
+    void Print() const;
+
+private:
+    string name;
+};
+
+#endif
