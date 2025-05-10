a93afa4ef22563861ca43f31243966a41c70b55f
FearlessRebelliousBoss <simplywonderfuld@gmail.com>
2025-05-10 16:22:15 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 #include "Name.h"
 #include "Andrejs.h"
+#include "Dana.h"
 using namespace std;
 
 int main()
 {
-	const int array_size = 1;
+	const int array_size = 2;
 	Name *Names[array_size];
 	
 	Andrejs a;
 	a.setName("Andrejs");
+
+	Dana d;
+        d.setName("Dana");
 	
 	Names[0] = &a;
+	Names[1] = &d;
 	
 	for (int i=0; i<array_size; i++)
 		Names[i]->Print();
