c8b9f084322a0b26815e4a5d349199d746667d85
FearlessRebelliousBoss <simplywonderfuld@gmail.com>
2025-05-10 16:26:23 +0300

    Update Dana.cpp

--- Dana.cpp
+++ Dana.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include "Dana.h"
+
+using namespace std;
+
+Dana::Dana(string n)
+{
+    name = n;
+}
+
+void Dana::setName(string n)
+{
+    name = n;
+}
+
+void Dana::Print() const
+{
+    cout << name << endl;
+}
 
